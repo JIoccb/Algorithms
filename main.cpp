45a84fa5c3a651b35aa7afe046d04e5b563bf18b
Artyom Rashevsky <albatroz3000@yandex.ru>
2025-04-17 15:02:57 +0300

    add TIMER

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@ using namespace std;
 #define ull unsigned long long
 #define u32 uint32_t
 
-//#define _DEBUG
+//#define DEBUG
+//#define TIMER
 
 #define PI acos(-1)
 #define inf LONG_LONG_MAX
@@ -22,20 +23,27 @@ using namespace std;
 
 
 void solution() {
-
 }
 
 int main() {
+#ifdef TIMER
+    auto _clock_start = chrono::high_resolution_clock::now();
+#endif
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    #if defined _DEBUG
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-    int t;
-    cin >> t;
+#ifdef DEBUG
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
     while (t--) {
         solution();
     }
+#ifdef TIMER
+    cerr << "Executed in " << chrono::duration_cast<chrono::milliseconds>(
+        chrono::high_resolution_clock::now()
+        - _clock_start).count() << "ms." << endl;
+#endif
 }
